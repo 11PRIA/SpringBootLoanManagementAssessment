e8d1438ed317ceff38ebfcfeeecd3cc30e1618d9
Priyanka kumari <145893083+11PRIA@users.noreply.github.com>
2026-04-23 15:28:09 +0530

    Update LoanController.java

--- src/main/java/com/cg/controller/LoanController.java
+++ src/main/java/com/cg/controller/LoanController.java
@@ -10,7 +10,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/loans")
 public class LoanController {
-
+  Loggerlogger =LoggerFactory.getLogger(LoanController.class);
     @Autowired
     private LoanService loanService;
 
